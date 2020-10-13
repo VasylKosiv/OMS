3e6c4c3203c59d0861b09a54631bd314be39a57d
VasylKosiv <59533908+VasylKosiv@users.noreply.github.com>
2020-10-13 15:09:02 +0300

    Add JavaProject

---
+++ oms/scripts/Up.sql
@@ -0,0 +1,13 @@
+use omsDB;
+
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='1';
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='2';
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='3';
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='4';
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='5';
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='6';
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='7';
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='8';
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='9';
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='10';
+UPDATE `omsDB`.`Users` SET `IsUserActive`=1 WHERE `ID`='11';
